05c5ea0b6b52d55c1ac26e875956d956bcb2afde
Victor <victormartinsjob@gmail.com>
2023-06-24 13:24:28 -0300

    Entrega 2

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 /* Desenvolva seu código abaixo */
 
-function playRockPaperScissor (player1, player2) {
-    if (player1 === "Pedra" && player2 === "Tesoura") {
+function playRockPaperScissor(player1, player2) {
+    if ((player1 === "Pedra" && player2 === "Tesoura") ||
+        (player1 === "Papel" && player2 === "Pedra") ||
+        (player1 === "Tesoura" && player2 === "Papel")) {
         return "Jogador 1 venceu!"
-    } else if (player1 === "Pedra" && player2 === "Papel") {
-        return "Jogador 2 venceu!"
-    } else if (player1 === "Papel" && player2 === "Pedra") {
-        return "Jogador 1 venceu!"
-    } else if (player1 === "Papel" && player2 === "Tesoura") {
-        return "Jogador 2 venceu!"
-    } else if (player1 === "Tesoura" && player2 === "Papel") {
-        return "Jogador 1 venceu!"
-    } else if (player1 === "Tesoura" && player2 === "Pedra") {
-        return "Jogador 2 venceu!"
-    } else {
+    } if (player1 === player2) {
         return "Empate!"
+    } else {
+        return "Jogador 2 venceu!"
     }
 }
 
-const resultado = playRockPaperScissor (player1, player2);
+const resultado = playRockPaperScissor(player1, player2);
 console.log(resultado);
